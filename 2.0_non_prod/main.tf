b3044741116db555db616c6f48ae05edebb34a29
Alexander Samaniego <alexanderwsp@hotmail.com>
2024-04-23 01:58:36 -0500

    feat(aks): adding pipeline

--- 2.0_non_prod/main.tf
+++ 2.0_non_prod/main.tf
@@ -17,7 +17,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_plugin     = "azure"
     service_cidr       = "10.0.2.0/24"
     dns_service_ip     = "10.0.2.10"
-    docker_bridge_cidr = "172.17.0.1/16"
     load_balancer_sku  = "standard"
   }
 
